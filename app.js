ed59af0d2eccad6388d64138efc381b1cd30aa23
jmichelin <jmichelinphone@gmail.com>
2016-01-10 15:21:09 -0800

    [Progress] finishes intial Model creation in app.js

--- app.js
+++ app.js
@@ -3,3 +3,50 @@
 require('dbutils/connection.js');
 
 let Bookshelf = require('bookshelf')(knex);
+
+//user profile model
+let UserProfile = Bookshelf.Model.extend({
+  tableName: 'user_profile',
+
+  userVotes: function () {
+    return this.hasMany(UserVotes, 'user_id')
+  }
+  
+});
+
+//user vote model
+let UserVotes = Bookshelf.Model.extend({
+  tableName: 'user_vote',
+
+  userProfile: function () {
+    return this.belongsTo(UserProfile);
+  },
+
+  ballotOptions: function () {
+    return this.belongsTo(BallotOptions, 'ballot_option_id');
+  }
+
+});
+
+//ballot options model
+let BallotOptions = Bookshelf.Model.extend({
+  tableName: 'ballot_option',
+
+  userVotes: function () {
+    return this.hasMany(UserVotes, 'ballot_option_id');
+  }
+
+});
+
+//ballot model
+
+let Ballot = Bookshelf.Model.extend({
+  tableName: 'ballot',
+
+  hasTimestamps: true,
+
+  ballotOptions: function () {
+    return this.hasMany(BallotOptions, 'ballot_id');
+  }
+
+});
